87df5c2347c3f523d0636282981ca9046bbeee4d
goyproject <anggasetiawan2958@gmail.com>
2022-01-25 22:22:42 +0700

    first

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Todolist</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+            <h2 class="font-weight-bold mt-3 ml-2">
+				.List Tugas Harian
+			</h2>
+			<p class="ml-2">Klik tombol <button type="button" class="btn btn-info">+Tugas Baru</button> untuk Membuat Tugas Harian Baru<br>
+            Jangan Lupa untuk memeriksa tugas anda yang sudah terselesaikan</p>
+		</div>
+		<div class="col-md-6 mt-4">
+         <div class="float-left">
+             <p>Tampilkan Tugas</p>
+            <button type="button" class="btn btn-primary">Selesai</button>
+            <button type="button" class="btn btn-secondary">Belum Selesai</button>
+        </div>
+        <div class="float-right">
+            <p>Urutkan berdasarkan</p>
+            <button type="button" class="btn btn-secondary">Tanggal dibuat (terbaru)</button>
+        </div>
+		</div>
+	</div>
+	<div class="row mt-3">
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">01 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-white bg-warning rounded">02 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">03 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">03 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+	</div>
+	<div class="row mt-3">
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">04 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">05 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">06 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+		<div class="col-md-3">
+            <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-white bg-warning rounded">07 januari 2022</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
